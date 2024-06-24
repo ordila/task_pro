0288860043d815db2d2ba061f776d04ca427bd20
ordila <ordila99@gmail.com>
2024-06-24 19:17:51 +0300

    prepare for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { store, persistor } from './redux/index.js';
-import App from './App.jsx';
+import { store, persistor } from "./redux/index.js";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/goals-achiever-react">
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/task_pro">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
